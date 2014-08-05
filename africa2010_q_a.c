72bd829ab10c0dfa673a2a5a994a792122308db5
Xiang Wu <xiangw.ahu@gmail.com>
2014-08-05 17:22:49 +0800

    update

---
+++ africa2010_q_a.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+	int cc, cn;
+	scanf("%d", &cn);
+
+	for (cc=1; cc<=cn; cc++)
+	{
+		int sum, num;
+		int arr[2000];
+		int i,j;
+		scanf("%d%d", &sum, &num);
+
+		for (i=0; i<num; i++)
+			scanf("%d", arr+i);
+		for (i=0; i<num; i++)
+		{
+			for (j=i+1; j<num; j++)
+			{
+				if (arr[i]+arr[j] == sum)
+				{
+					printf("Case #%d: %d %d\n", cc, i+1, j+1);
+					goto END;
+				}
+			}
+		}
+
+END:
+		;
+	}
+
+	return 0;
+}
---
+++ beta2008_b.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+struct stru{
+	string s;
+	int val;
+	int id;
+};
+
+int ISubSqn(vector<stru> vec)
+{
+	vector<int> v;
+	v.resize(vec.size(), 1);
+	for (int i=1; i<vec.size(); i++){
+		for (int j=0; j<i; j++){
+			if (vec[j].id < vec[i].id && v[j]+1 > v[i])
+				v[i] = v[j]+1;
+		}
+	}
+
+	return *max_element(v.begin(), v.end());
+}
+
+int main()
+{
+	int cn, cc;
+	cin >> cn;
+
+	for (cc=1; cc<=cn; cc++)
+	{
+		vector<stru> vec;
+		cin.get();
+		do
+		{
+			stru st;
+			vec.push_back(st);
+			cin >> vec.back().s;
+			vec.back().id = vec.size();
+		}
+		while (cin.get() != '\n');
+		for (int i=0; i<vec.size(); i++)
+			cin >> vec[i].val;
+		cout << "Case #" << cc << ":";
+
+		sort(vec.begin(), vec.end(), [](const stru a, const stru b)->bool { return a.val < b.val; });
+
+		int reachlen = ISubSqn(vec);
+		while (vec.size() > reachlen)
+		{
+			vector<stru>::iterator tmax = vec.begin();
+			for (vector<stru>::iterator i = vec.begin(); i != vec.end(); i++){
+				if (tmax->s.compare(i->s) > 0)
+					tmax = i;
+			}
+			stru tmp = *tmax;
+			vec.erase(tmax);
+			if (ISubSqn(vec) >= reachlen)
+				cout << " " << tmp.s;
+			else
+			{
+				tmp.s = " ";
+				tmp.s[0] = 'z'+1;
+				vec.push_back(tmp);
+				sort(vec.begin(), vec.end(), [](const stru a, const stru b)->bool { return a.val < b.val; });
+			}
+		}
+
+		cout << endl;
+	}
+	return 0;
+}
