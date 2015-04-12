c183fda784cbb59a0e76e86177280670e86127b0
Xiang Wu <xiangw.ahu@gmail.com>
2015-04-12 18:26:09 +0800

    add cpp templet

---
+++ templet.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int cn; cin >> cn;
+	for (int cc=1; cc<=cn; cc++)
+	{
+		
+
+		cout << "Case #" << cc << ": " << endl;
+	}
+	return 0;
+}
